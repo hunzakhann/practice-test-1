e3c632edef9ed11eb864ff8b704296dd8a506a16
Hunza Khan <hunzakhan2098@gmail.com>
2023-03-18 13:48:56 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+var english = 30
+var urdu = 40 
+var math = 50
+
+var sumofmarks = english + urdu + math;
+// alert(sumofmarks);
+ 
+var percentage = sumofmarks/300
+console.log(percentage)
+if(percentage > 50){
+document.write("passs")
+}else{
+    document.write("fale")
+}
+
+// var city = prompt("this is city")
+// if(city === karachi){
+//  alert("correct")
+// }
